3155fe3a989a1a70686a2d56e4ff36a643079e8c
Muneeb Ur Rehman <137985020+Muneebsial0321@users.noreply.github.com>
2026-06-14 16:13:52 +0500

    feat:nativewind added successfully

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,7 @@ import { Text, View } from "react-native";
  
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
+    <View className="flex-1 items-center justify-center bg-purple-500">
       <Text className="text-xl font-bold text-blue-500">
         Welcome to Nativewind!
       </Text>
