b010607ae15e83efe2a2e656905721db6f8c2414
Muneeb Ur Rehman <137985020+Muneebsial0321@users.noreply.github.com>
2026-06-14 16:58:45 +0500

    fix: dep issues sorted

--- babel.config.js
+++ babel.config.js
@@ -5,6 +5,6 @@ module.exports = function (api) {
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
     ],
-    plugins: ["drizzle-orm/babel-plugin"],
+    plugins: [["inline-import", { "extensions": [".sql"] }]] 
   };
 };
