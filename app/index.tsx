b9f8f984874227b997a84bc7e2f0357b93239d71
Muneeb Ur Rehman <137985020+Muneebsial0321@users.noreply.github.com>
2026-06-14 16:26:07 +0500

    feat:rn-reusables added successfully

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,18 @@
-import { Text, View } from "react-native";
+import { Button } from "@/src/components/ui/button";
+import { Text } from "@/src/components/ui/text";
+import { View } from "react-native";
  
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-purple-500">
+    <View className="flex-1 items-center justify-center">
       <Text className="text-xl font-bold text-blue-500">
         Welcome to Nativewind!
       </Text>
+      <Button className="w-full bg-black active:bg-black active:scale-90 transition-all duration-75">
+        <Text className="text-white">
+          Click me!
+        </Text>
+      </Button>
     </View>
   );
 }
